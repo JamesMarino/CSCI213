cec316c006fd66bf7644bff01bffbe0d7d89c3f2
James Marino <jamesmarinoau@gmail.com>
2015-08-25 12:09:56 +1000

    Task 1 Done

--- Assignments/Assignment 1/Solutions/src/au/edu/uow/UserInterface/Student.java
+++ Assignments/Assignment 1/Solutions/src/au/edu/uow/UserInterface/Student.java
@@ -1,24 +1,36 @@
 package au.edu.uow.UserInterface;
 
-public class Student
-{
+public class Student {
+    // Properties
+    private int Score;
+    private String Name;
+
+    Student()
+    {
+        // Set Initial Values
+        Score = 0;
+        Name = "";
+    }
+
     public void recordScore(boolean isCorrect)
     {
-
+        if (isCorrect) {
+            Score += 1;
+        }
     }
 
     public void setName(String name)
     {
-
+        Name = name;
     }
 
     public String getName()
     {
-        return "N/A";
+        return Name;
     }
 
     public  int getScore()
     {
-        return  0;
+        return Score;
     }
 }
